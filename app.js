830dff567246ea4f107b7b96d6b238c33caceb65
xabibullayevuser <xusniddinxabibullayev25@gmail.com>
2024-05-14 23:49:04 +0500

    ERP

---
+++ app.js
@@ -0,0 +1,9 @@
+const from = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    name; form.name.value,
+    phoneNumber; form["phone-number"].value,
+    userImage; form.image.value
+    // email; form.email.value, 
+});
